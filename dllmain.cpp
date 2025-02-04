e81f4cd2b1c9a8e7b8fccb264e2967f6f331904f
Connor J Thompson <ct770a@gmail.com>
2025-02-04 13:08:24 +1100

    changed outputs to be a little less interuptive

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,24 +1,26 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <iostream>
+using namespace std;
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
     switch (ul_reason_for_call){
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(0, "DLL Alert: Attached!", "Epic", MB_OK);
+        cout << "[Injected DLL] attached\n";
         break;
     case DLL_THREAD_ATTACH:
-        MessageBoxA(0, "DLL Alert: Thread attached!", "Epic", MB_OK);
+        cout << "[Injected DLL] thread attached\n";
         break;
     case DLL_THREAD_DETACH:
-        MessageBoxA(0, "DLL Alert: Thread detached!", "Sad", MB_OK);
+        cout << "[Injected DLL] thread detached\n";
         break;
     case DLL_PROCESS_DETACH:
-        MessageBoxA(0, "DLL Alert: Detached!", "Sad", MB_OK);
+        cout << "[Injected DLL] detached\n";
         break;
     }
     return TRUE;
 }
 
 extern "C" __declspec(dllexport) void DLLRun(void* data_globals) {
-    MessageBoxA(0, "DLL Alert: DLL has been run!", "Epic", MB_OK);
+    cout << "[Injected DLL] run called\n";
 }
