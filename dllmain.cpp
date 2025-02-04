4c2f327199132c8f8367b73c56b5af28a6b05cd4
Connor J Thompson <ct770a@gmail.com>
2025-02-04 20:04:31 +1100

    added in a globals data object, which we can simply use to communicate directly to this moddy dll with via assembly patches from the graphics injector

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,8 +1,13 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include <iostream>
+#include "globals.h"
 using namespace std;
 
+DLLGLobals globals = {};
+extern "C" __declspec(dllexport) void* DLLGlobals() {
+    return &globals;
+}
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
     switch (ul_reason_for_call){
     case DLL_PROCESS_ATTACH:
@@ -21,6 +26,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
     return TRUE;
 }
 
-extern "C" __declspec(dllexport) void DLLRun(void* data_globals) {
-    cout << "[Injected DLL] run called\n";
+extern "C" __declspec(dllexport) void DLLRun() {
+    cout << "[Injected DLL] run called, params: " << globals.debug1 << ", " << globals.debug2 << ", " << globals.debug3 << ", " << globals.debug4 << ", ";
 }
+
