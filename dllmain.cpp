c65bb19c303af178bd96c9567e07cfa2de49e8cf
Connor J Thompson <ct770a@gmail.com>
2025-02-05 22:00:41 +1100

    added hook to reroute all window process calls to our moddy dll as well

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,10 +8,29 @@ DLLGLobals globals = {};
 extern "C" __declspec(dllexport) void* DLLGlobals() {
     return &globals;
 }
+
+bool has_injected = false;
+int WndProcHook(int code, WPARAM wParam, LPARAM lParam) {
+    MSG* windows_event = (MSG*)lParam;
+
+    //cout << "[Injected DLL] message intercepted, code: " << windows_event->message << ", params: " << windows_event->wParam << ", " << windows_event->lParam << endl;
+
+    return(CallNextHookEx(NULL, code, wParam, lParam));
+}
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
+    
+
     switch (ul_reason_for_call){
     case DLL_PROCESS_ATTACH:
         cout << "[Injected DLL] attached\n";
+        // then run attach logic
+        if (!has_injected) {
+            has_injected = true;
+            cout << "[Injected DLL] initiating windows event hook\n";
+
+            HHOOK handle = SetWindowsHookExA(WH_GETMESSAGE, (HOOKPROC)WndProcHook, hModule, GetCurrentThreadId());
+            if (!handle) cout << "[Injected DLL] failed to hook windows event function\n";
+        }
         break;
     case DLL_THREAD_ATTACH:
         cout << "[Injected DLL] thread attached\n";
@@ -27,6 +46,6 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 }
 
 extern "C" __declspec(dllexport) void DLLRun() {
-    cout << "[Injected DLL] run called, params: " << globals.debug1 << ", " << globals.debug2 << ", " << globals.debug3 << ", " << globals.debug4 << ", ";
+    cout << "[Injected DLL] run called, params: " << globals.debug1 << ", " << globals.debug2 << ", " << globals.debug3 << ", " << globals.debug4 << "\n";
 }
 
