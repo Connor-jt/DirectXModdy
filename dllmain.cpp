467c2514f5fb6a35ab0dab57b40a699a981b922b
Connor J Thompson <ct770a@gmail.com>
2025-02-08 12:24:47 +1100

    updated our window proc event hook thing to use a much more reliable system, where it literally just sets a new pointer to call

--- dllmain.cpp
+++ dllmain.cpp
@@ -23,7 +23,9 @@ DLLGLobals globals = {};
 extern "C" __declspec(dllexport) void* DLLGlobals() { return &globals; }
 
 HWND target_hwnd = 0;
-DWORD target_thread = 0;
+//DWORD target_thread = 0;
+
+WNDPROC OldWndProc = 0;
 
 ID3D11Device1* dx_device = nullptr;
 ID3D11DeviceContext1* dx_device_context = nullptr;
@@ -59,7 +61,9 @@ void init_graphics(ID3D11DeviceContext1* _dx_device_context) {
     ImGui_ImplWin32_Init(target_hwnd);
     ImGui_ImplDX11_Init(dx_device, dx_device_context);
 }
-extern "C" __declspec(dllexport) void DLLRun(IDXGISwapChain1 * swap_chain, UINT SyncInterval, UINT Flags) {
+
+LRESULT CALLBACK WndProcHook2(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+extern "C" __declspec(dllexport) void DLLRun(IDXGISwapChain1* swap_chain, UINT SyncInterval, UINT Flags) {
     //cout << "[Injected DLL] device: " << dx_device << " device context: " << dx_device_context << " global context: " << globals.last_d3d11DeviceContext << "\n";
     //cout << "[Injected DLL] globals ptr: " << &globals << "\n";
     // fail if bad swapchain
@@ -72,13 +76,14 @@ extern "C" __declspec(dllexport) void DLLRun(IDXGISwapChain1 * swap_chain, UINT 
         DXGI_SWAP_CHAIN_DESC swapChainDesc;
         swap_chain->GetDesc(&swapChainDesc);
         target_hwnd = swapChainDesc.OutputWindow;
+        OldWndProc = (WNDPROC)SetWindowLongPtrA(target_hwnd, GWLP_WNDPROC, (LONG_PTR)WndProcHook2);
         cout << "[Injected DLL] hwnd: " << target_hwnd << " swap chain: " << swap_chain << "\n";
     }
     // fail run if no window handle
-    if (!target_hwnd || !target_thread) {
-        cout << "[Injected DLL] hwnd: " << target_hwnd << " thread: " << target_thread << "\n";
-        return;
-    }
+    //if (!target_hwnd || !target_thread) {
+    //    cout << "[Injected DLL] hwnd: " << target_hwnd << " thread: " << target_thread << "\n";
+    //    return;
+    //}
 
     // init graphics if we haven't yet, and the other hooks have fetched the ptr
     if (!dx_device_context) {
@@ -96,31 +101,31 @@ extern "C" __declspec(dllexport) void DLLRun(IDXGISwapChain1 * swap_chain, UINT 
     }
 
 
-    cout << "[Injected DLL] rendering 1\n";
+    //cout << "[Injected DLL] rendering 1\n";
     ImGui_ImplDX11_NewFrame();
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
 
-    cout << "[Injected DLL] rendering 2\n";
+    //cout << "[Injected DLL] rendering 2\n";
     ImGui::Begin("Another Window"); 
     ImGui::Text("Hello from another window!");
-    cout << "[Injected DLL] rendering 3\n";
+    //cout << "[Injected DLL] rendering 3\n";
     ImGui::End();
 
-    cout << "[Injected DLL] rendering 4\n";
+    //cout << "[Injected DLL] rendering 4\n";
     ImGui::Render();
-    cout << "[Injected DLL] rendering 5\n";
+    //cout << "[Injected DLL] rendering 5\n";
     ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-    cout << "[Injected DLL] rendering 6\n";
+    //cout << "[Injected DLL] rendering 6\n";
 
     RECT rect;
     if (GetWindowRect(target_hwnd, &rect)){
         int width = rect.right - rect.left;
         int height = rect.bottom - rect.top;
-        cout << "[Injected DLL] run called, tID: " << GetCurrentThreadId() << " target_thread: " << target_thread << " width: " << width << " height: " << height << "\n";
+        cout << "[Injected DLL] run called, tID: " << GetCurrentThreadId() << " width: " << width << " height: " << height << "\n";
     }
     else {
-        cout << "[Injected DLL] run called, tID: " << GetCurrentThreadId() << " target_thread: " << target_thread << "\n";
+        cout << "[Injected DLL] run called, tID: " << GetCurrentThreadId() << "\n";
     }
 
     //cout << "[Injected DLL] run called, params: " << globals.debug1 << ", " << globals.debug2 << ", " << globals.debug3 << ", " << globals.debug4 << "\n";
@@ -129,56 +134,25 @@ extern "C" __declspec(dllexport) void DLLRun(IDXGISwapChain1 * swap_chain, UINT 
 
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-int WndProcHook(int code, WPARAM wParam, LPARAM lParam) {
-    MSG* event = (MSG*)lParam;
-    if (ImGui_ImplWin32_WndProcHandler(event->hwnd, event->message, event->wParam, event->lParam))
-        goto return_consumed;
+LRESULT CALLBACK WndProcHook2(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) return 0;
 
-    switch (event->message) {
+    switch (msg) {
     case WM_KEYDOWN:
     case WM_KEYUP:
     case WM_MOVE:
         break;
     }
-    
 
-    goto return_unconsumed;
-return_consumed:
-    event->message = WM_NULL;
-return_unconsumed:
-    return(CallNextHookEx(NULL, code, wParam, lParam));
+    return CallWindowProcA(OldWndProc, hwnd, msg, wparam, lparam);
 }
 
-
-// DLL INIT STUFF
-void InitEventsHook() {
-    // find the processes main thread (we just get whatever window thread they have open)
-    auto EnumWindowsProc = [](HWND hwnd, LPARAM lParam) -> BOOL {
-        DWORD windowProcessId;
-        DWORD thread_id = GetWindowThreadProcessId(hwnd, &windowProcessId);
-        if (windowProcessId == GetCurrentProcessId()) {
-            //target_hwnd = hwnd;
-            target_thread = thread_id;}
-        return TRUE;};
-
-    EnumWindows(EnumWindowsProc, 0);
-    if (!target_thread) { MessageBoxA(0, "failed to find window to hook to", "Hook failure", 0); return;}
-
-    if (!SetWindowsHookExA(WH_GETMESSAGE, (HOOKPROC)WndProcHook, 0, target_thread)) {
-        MessageBoxA(0, "failed to set windows proc hook", "Hook failure", 0);
-        return;}
-
-    // pause indefinitely, because if this thread closes then for some reason it also closes the hook (and as you can tell with the suspension, this thread has literally no involvement??)
-    SuspendThread(GetCurrentThread());
-}
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved){
-    if (GetEnvironmentVariableA("DLL_DO_NOT_INIT", 0, 0)) return TRUE;
-    switch (ul_reason_for_call){
-    case DLL_PROCESS_ATTACH:{
-        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)InitEventsHook, 0, 0, 0);
-        break;}
+    //if (GetEnvironmentVariableA("DLL_DO_NOT_INIT", 0, 0)) return TRUE;
+    //switch (ul_reason_for_call){
+    //case DLL_PROCESS_ATTACH:
     //case DLL_THREAD_ATTACH:
     //case DLL_THREAD_DETACH:
     //case DLL_PROCESS_DETACH:
-    } return TRUE;
+    //} return TRUE;
 }
