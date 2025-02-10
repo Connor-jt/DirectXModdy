0762dec3eb6224d69281b41df5df61848f27c981
Connor J Thompson <ct770a@gmail.com>
2025-02-10 22:06:19 +1100

    cleaned up the drawindexed assembly function hook + removed excess globals data

--- globals.h
+++ globals.h
@@ -6,9 +6,4 @@ public:
     void* last_d3d11DeviceContext;
     void* last_ID3D11Buffer; // from VSSetConstantBuffers
     void* last_ID3D11VertexShader; // from VSSetShader
-
-    unsigned long long debug1; // func 1 rcx
-    unsigned long long debug2; // func 1 incrementor
-    unsigned long long debug3; // func 3 access count
-    unsigned long long debug4; // func 4 access count
 };
